f8c13860742cfa3b9344cf6b773b7ef49f0788be
Štěpán Beneš <benes.stepan@gmail.com>
2022-02-06 11:55:42 +0100

    Handle ctrl_c and SIGTERM signals asynchronously

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //#![warn(rust_2018_idioms)]
 
+use tokio::signal::{ctrl_c, unix::{signal, SignalKind}};
 use futures::stream::StreamExt;
 use std::{
     env,
@@ -55,34 +56,25 @@ async fn main() -> tokio_serial::Result<()> {
     port.set_exclusive(false)
         .expect("Unable to set serial port exclusive to false");
 
-    let mut io = Framed::new(port, LinesCodec::new()); //LineCodec.framed(port);
+    let mut io = Framed::new(port, LinesCodec::new());
 
-    // while let Some(line_result) = io.next().await {
-    //     let line = line_result.expect("Failed to read line");
-    //     println!("{}", line);
-    //     io.send("pi".to_string()).await.expect("Failed to send text");
-    // }
+    //let stdin = tokio::io::stdin();
+    //let mut reader = FramedRead::new(stdin, LinesCodec::new()); // stdin is blocking and prevents shutdown
 
-    // let line = do_the_line().await.expect("Failed to read line from stdin");
-    // println!("stdin: {}", line);
-
-    //println!("hello");
-
-    let stdin = tokio::io::stdin();
-    let mut reader = FramedRead::new(stdin, LinesCodec::new());
-
-    let mut controllers: Vec<_> = Vec::<Controller>::new();
+    //let mut controllers: Vec<_> = Vec::<Controller>::new();
     
-    let mut listener = Listener::default();
+    //let mut listener = Listener::default();
 
     // TODO: works only for connected gamepad and first three devices, otherwise crashes on assert (incompattible device)
     // TODO: adapt implementation to create controller instance from device address or name
     // see: https://github.com/libcala/stick/blob/ab3bdd7746a19b0319e21a246e3e66bdd4882f70/stick/src/raw/linux.rs#L745
     // see: https://github.com/libcala/stick/blob/ab3bdd7746a19b0319e21a246e3e66bdd4882f70/stick/src/raw/linux.rs#L762
     // see: https://github.com/libcala/stick/blob/ab3bdd7746a19b0319e21a246e3e66bdd4882f70/stick/src/raw/linux.rs#L785
-    controllers.push((&mut listener).await); // gamepad buttons and axes
-    controllers.push((&mut listener).await); // gamepad motion
-    controllers.push((&mut listener).await); // gamepad touchpad
+    //controllers.push((&mut listener).await); // gamepad buttons and axes
+    //controllers.push((&mut listener).await); // gamepad motion
+    //controllers.push((&mut listener).await); // gamepad touchpad
+
+    let mut sigterm_stream = signal(SignalKind::terminate())?;
 
     loop {
         tokio::select! {
@@ -90,32 +82,41 @@ async fn main() -> tokio_serial::Result<()> {
                 let line = serial_line.expect("Failed to read line from serial");
                 println!("serial: {}", line);
             },
-            stdin_line = read_stdin_line(&mut reader) => {
-                let line = stdin_line.expect("Failed to read line from stdin");
-                println!("stdin: {}", line);
-                //io.send(line).await.expect("Failed to send text");
+            // stdin_line = read_stdin_line(&mut reader) => {
+            //     let line = stdin_line.expect("Failed to read line from stdin");
+            //     println!("stdin: {}", line);
+            //     io.send(line).await.expect("Failed to send text");
+            // },
+            _ = ctrl_c() => {
+                println!("Received ctrl+c. Shutting down.");
+                break;
             },
-            event = &mut controllers[0] => {
-                println!("{:?}", event);
-                match event {
-                    Event::Disconnect => {
-                    }
-                    Event::ActionA(pressed) => {
-                        controllers[0].rumble(1.0f32);
-                    }
-                    Event::ActionB(pressed) => {
-                        io.send(format!("{}", pressed)).await.expect("Failed to send text");
-                        //controller.rumble(f32::from(u8::from(pressed)));
-                    }
-                    Event::BumperL(pressed) => {
+            _ = sigterm_stream.recv() => {
+                println!("Received SIGTERM. Shutting down.");
+                break;
+            },
+            
+            // event = &mut controllers[0] => {
+            //     println!("{:?}", event);
+            //     match event {
+            //         Event::Disconnect => {
+            //         }
+            //         Event::ActionA(pressed) => {
+            //             controllers[0].rumble(1.0f32);
+            //         }
+            //         Event::ActionB(pressed) => {
+            //             io.send(format!("{}", pressed)).await.expect("Failed to send text");
+            //             //controller.rumble(f32::from(u8::from(pressed)));
+            //         }
+            //         Event::BumperL(pressed) => {
                         
-                    }
-                    Event::BumperR(pressed) => {
+            //         }
+            //         Event::BumperR(pressed) => {
 
-                    }
-                    _ => {}
-                }
-            },
+            //         }
+            //         _ => {}
+            //     }
+            // },
         }
         println!("---");
     }
@@ -123,9 +124,9 @@ async fn main() -> tokio_serial::Result<()> {
     Ok(())
 }
 
-async fn read_stdin_line(reader: &mut FramedRead<tokio::io::Stdin, LinesCodec>) -> Result<String, Box<dyn std::error::Error>> {
-    Ok(reader.next().await.transpose()?.unwrap())
-}
+// async fn read_stdin_line(reader: &mut FramedRead<tokio::io::Stdin, LinesCodec>) -> Result<String, Box<dyn std::error::Error>> {
+//     Ok(reader.next().await.transpose()?.unwrap())
+// }
 
 async fn read_serial_line(
     io: &mut Framed<SerialStream, LinesCodec>,
