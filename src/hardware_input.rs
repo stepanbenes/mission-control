173313b86d40b959b6695a61470d397f5bb4f166
Štěpán Beneš <benes.stepan@gmail.com>
2022-05-20 13:50:12 +0200

    Add comment aboout interrupts

--- src/hardware_input.rs
+++ src/hardware_input.rs
@@ -9,6 +9,7 @@ impl HardwareInput {
 		let gpio = rppal::gpio::Gpio::new()?;
 
 		let mut button_pin = gpio.get(4)?.into_input_pullup();
+		// TODO: use sync interrupt on separate thread instead
 		button_pin.set_async_interrupt(Trigger::FallingEdge, |_| println!("Button pressed!"))?;
 
 		Ok(Self { button_pin })
